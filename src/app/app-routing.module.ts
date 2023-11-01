1e147dbaa4057dfedd39335e82a2a88f9fc1ebca
Rafael Rodrigues <rafars18@hotmail.com>
2023-11-01 09:00:39 -0300

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HomeComponent } from './home/home.component';
+import { ApoliceSeguroComponent } from './apolice-seguro/apolice-seguro.component';
+import { TelaPaiComponent } from './tela-pai/tela-pai.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { CalcularMediaComponent } from './calcular-media/calcular-media.component';
+import { TelaFilho1Component } from './tela-pai/tela-filho1/tela-filho1.component';
+import { TelaFilho2Component } from './tela-pai/tela-filho2/tela-filho2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:"calculadora", component: CalculadoraComponent},
+  {path:"calcular-media", component:CalcularMediaComponent},
+  {path:"home", component: HomeComponent},
+  {path:"apolice-seguro", component: ApoliceSeguroComponent},
+  {path:"tela-pai", component: TelaPaiComponent,
+    children:[ 
+      {path: 'tela-filho1', component: TelaFilho1Component},
+      {path: 'tela-filho2', component: TelaFilho2Component}
+  ]},
+  {path:'**', component: PaginaNaoEncontradaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,24 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { CalcularMediaComponent } from './calcular-media/calcular-media.component';
 import { ApoliceSeguroComponent } from './apolice-seguro/apolice-seguro.component';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { TelaPaiComponent } from './tela-pai/tela-pai.component';
+import { HomeComponent } from './home/home.component';
+import { TelaFilho1Component } from './tela-pai/tela-filho1/tela-filho1.component';
+import { TelaFilho2Component } from './tela-pai/tela-filho2/tela-filho2.component';
 @NgModule({
   declarations: [
     AppComponent,
     CalculadoraComponent,
     CalcularMediaComponent,
-    ApoliceSeguroComponent
+    ApoliceSeguroComponent,
+    MenuSuperiorComponent,
+    PaginaNaoEncontradaComponent,
+    TelaPaiComponent,
+    HomeComponent,
+    TelaFilho1Component,
+    TelaFilho2Component
   ],
   imports: [
     BrowserModule,
